293b73295272498e03dade6ee6e014058113cdcf
George Panov <georgepanov19@gmail.com>
2026-09-19 14:40:17 +0300

    feat: добавляет alias и perfectionist

--- eslint.config.js
+++ eslint.config.js
@@ -1,5 +1,6 @@
 import js from '@eslint/js';
 import globals from 'globals';
+import perfectionist from 'eslint-plugin-perfectionist';
 import reactHooks from 'eslint-plugin-react-hooks';
 import reactRefresh from 'eslint-plugin-react-refresh';
 import tseslint from 'typescript-eslint';
@@ -15,8 +16,50 @@ export default defineConfig([
       reactHooks.configs.flat.recommended,
       reactRefresh.configs.vite,
     ],
+    plugins: {
+      perfectionist,
+    },
     languageOptions: {
       globals: globals.browser,
+    },
+    rules: {
+      'perfectionist/sort-imports': [
+        'warn',
+        {
+          type: 'natural',
+          order: 'asc',
+
+          groups: [
+            'type',
+            'builtin',
+            'external',
+            'app',
+            'components',
+            'shared',
+            'internal',
+            ['parent', 'sibling', 'index'],
+            'side-effect',
+            'unknown',
+          ],
+
+          customGroups: [
+            {
+              groupName: 'app',
+              elementNamePattern: '~/app/',
+            },
+            {
+              groupName: 'components',
+              elementNamePattern: '~/components/',
+            },
+            {
+              groupName: 'shared',
+              elementNamePattern: '~/shared/',
+            },
+          ],
+        },
+      ],
+
+      '@typescript-eslint/no-unused-vars': ['warn'],
     },
   },
 ]);
